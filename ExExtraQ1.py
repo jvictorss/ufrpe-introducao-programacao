fe2b2f6ca41857c5bbb41b9805fbf592fcde97c5
João Victor <joaovss1992@gmail.com>
2022-03-19 01:15:02 -0300

    Adicionando lógica

---
+++ ExExtraQ1.py
@@ -0,0 +1,14 @@
+#Resolva as expressões lógicas, determinando se a expressão é verdadeira ou falsa:
+# A: 2>3
+#B: (6<8) ou (3>7)
+#C: não (2<3)
+#D: (5>=6 ou 6<7 ou não(a+5-6=8) onde a = 5
+#E: (34>9 e 5+u = 34) ou (5=15/3 e 8>12) = ((u = 29) e (8>12)
+
+a = 2
+b = 3
+if a > b:
+    print("true")
+else:
+    print("false")
+
